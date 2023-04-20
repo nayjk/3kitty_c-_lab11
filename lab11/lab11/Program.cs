6ec00873838fda48e6c505685bf7cab35c0842d0
Nayjk <98636064+nayjk@users.noreply.github.com>
2023-04-20 20:10:26 +0300

    Add files via upload

---
+++ lab11/lab11/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace lab11
+{
+    using System;
+
+    delegate string GetWeekDay();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] weekDaysRussianNames = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+
+            int currentDay = 0;
+
+
+            GetWeekDay getWeekDay = () =>
+            {
+                int day = currentDay % 7;
+                currentDay++;
+                return weekDaysRussianNames[day];
+            };
+
+            
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine(getWeekDay());
+            }
+        }
+    }
+}
